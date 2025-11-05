51710b3e403ee6766b7520e96ff4801946bca9e5
Mohammed Elhawary <elhawaryh0@gmail.com>
2025-11-05 18:33:49 +0200

    first commet

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_screen.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      routes: {HomeScreen.id: (context) => HomeScreen()},
+      initialRoute: HomeScreen.id,
+    );
+  }
+}
