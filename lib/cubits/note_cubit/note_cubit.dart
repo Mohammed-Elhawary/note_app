e422578345d1d2d69145f96369c7fc3bd934c625
Mohammed Elhawary <elhawaryh0@gmail.com>
2025-11-30 15:22:01 +0200

    finsh method featch all notes in NoteCubit

--- lib/cubits/note_cubit/note_cubit.dart
+++ lib/cubits/note_cubit/note_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'note_state.dart';
 
 class NoteCubit extends Cubit<NoteState> {
   NoteCubit() : super(NoteInitial());
+
+  featchAllNote() async {
+    emit(NoteLoading());
+    try {
+      var notes = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> listNote = notes.values.toList();
+      emit(NoteSucsses(notes: listNote));
+    } catch (e) {
+      emit(NoteFailure(errMsg: e.toString()));
+    }
+  }
 }
